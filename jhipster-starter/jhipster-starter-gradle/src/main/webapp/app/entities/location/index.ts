11bd0245fcb10472707eb4cad2dedbf98ce8e3af
Zemian Deng <zemiandeng@gmail.com>
2019-03-30 12:10:01 -0400

    Move microservices projects into separate repo

--- jhipster-starter/jhipster-starter-gradle/src/main/webapp/app/entities/location/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './location.service';
-export * from './location-update.component';
-export * from './location-delete-dialog.component';
-export * from './location-detail.component';
-export * from './location.component';
-export * from './location.route';
--- jhipster-starter/jhipster-starter-maven/src/main/webapp/app/entities/author/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './author.service';
-export * from './author-update.component';
-export * from './author-delete-dialog.component';
-export * from './author-detail.component';
-export * from './author.component';
-export * from './author.route';
