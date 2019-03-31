69c80b3fc42b6f419f7cc506d42c4fb5c815f712
Zemian Deng <zemiandeng@gmail.com>
2019-03-31 10:46:17 -0400

    Added comment on debug

--- java8-starter/src/main/java/zemian/javastarter/serviceloader/MyServiceDemo.java
+++ java8-starter/src/main/java/zemian/javastarter/serviceloader/MyServiceDemo.java
@@ -1,8 +1,18 @@
 package zemian.javastarter.serviceloader;
 
 import java.util.Iterator;
+import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
+/*
+A demo to see how ServiceLoader works.
+
+Setup: META-INF/services/<ServiceInterfaceName> => single line of impl class
+
+NOTE: Service implementation must have a default no-arg constructor.
+
+See https://docs.oracle.com/javase/7/docs/api/java/util/ServiceLoader.html
+ */
 public class MyServiceDemo {
     public static void main(String[] args) {
         main2(args);
@@ -17,12 +27,18 @@ public class MyServiceDemo {
     }
 
     public static void main2(String[] args) {
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        ServiceLoader<MyService> loader = ServiceLoader.load(MyService.class, cl);
-        Iterator<MyService> iter = loader.iterator();
-        if (!iter.hasNext())
-            return;
-        MyService provider = iter.next();
-        System.out.println(provider);
+        try {
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            ServiceLoader<MyService> loader = ServiceLoader.load(MyService.class, cl);
+            Iterator<MyService> iter = loader.iterator();
+            if (!iter.hasNext())
+                return;
+            MyService provider = iter.next();
+            System.out.println(provider);
+        } catch (ServiceConfigurationError ignore) {
+            // How do we debug "ignore" if third party lib choose to do this?
+            // Answer: In IDEA, place breakpoint before exception, then open
+            // breakpoint view, and enable "Any Exception", then step through.
+        }
     }
 }
--- java8-starter/src/main/java/zemian/javastarter/serviceloader/MyServiceImplA.java
+++ java8-starter/src/main/java/zemian/javastarter/serviceloader/MyServiceImplA.java
@@ -1,6 +1,10 @@
 package zemian.javastarter.serviceloader;
 
 public class MyServiceImplA implements MyService {
+    public MyServiceImplA(String param) {
+        System.out.println(param);
+    }
+
     @Override
     public void run() {
         System.out.println("MyServiceImplA");
