4277918284f66939b9eaafdea904e96327798753
Zemian Deng <zemian.deng@oracle.com>
2019-12-08 10:36:58 -0500

    Rename from java8-starter

--- java8-starter/src/main/java/zemian/javastarter/serviceloader/MyServiceImplB.java
+++
@@ -1,8 +0,0 @@
-package zemian.javastarter.serviceloader;
-
-public class MyServiceImplB implements MyService {
-    @Override
-    public void run() {
-        System.out.println("MyServiceImplB");
-    }
-}
