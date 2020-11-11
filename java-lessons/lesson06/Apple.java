4e131b52a46c3d13d0d80e092af26971b8b91d56
Zemian Deng <zemiandeng@gmail.com>
2020-11-11 12:05:48 -0500

    Add lesson 6

---
+++ java-lessons/lesson06/Apple.java
@@ -0,0 +1,8 @@
+package lesson06;
+
+// This class definition is inside the "package" named "lesson6".
+public class Apple {
+    public static String tasteDescription() {
+        return "Apple is Crunchy, but it can be made into a great Pie!";
+    }
+}
---
+++ java-lessons/lesson06/recipe/ApplePie.java
@@ -0,0 +1,9 @@
+package lesson06.recipe;
+
+public class ApplePie {
+    public static String howToCook() {
+        return "1. Peel Apple\n" +
+                "2. Beat Egg\n" +
+                "3. Bake it";
+    }
+}
